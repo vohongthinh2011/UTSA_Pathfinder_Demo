f17d2fb1a7222c8c7f730ba74724945ce8fda3a1
Thinh H Vo <vohongthinh2011@gmail.com>
2016-09-21 16:07:48 -0500

    Added a space

--- app/src/main/java/com/itshareplus/googlemapdemo/MapsActivity.java
+++ app/src/main/java/com/itshareplus/googlemapdemo/MapsActivity.java
@@ -46,7 +46,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     //test
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+        super.onCreate(savedInstanceState );
         setContentView(R.layout.activity_maps);
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
